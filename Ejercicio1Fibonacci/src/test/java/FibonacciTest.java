93d69c92822514d765faf2eb28ab51740791d0fc
Arthurcep <arthureboh@gmail.com>
2022-03-09 19:44:00 +0100

    Añadidas mas pruebas

--- Ejercicio1Fibonacci/src/test/java/FibonacciTest.java
+++ Ejercicio1Fibonacci/src/test/java/FibonacciTest.java
@@ -14,12 +14,45 @@ public class FibonacciTest
 
 
     @Test
+    public void fibonacciDeCeroEsCero()
+    {
+        Fibonacci fibonacci = new Fibonacci();
+
+        int expectedValue = 0;
+        int obtainedValue = fibonacci.compute(0);
+
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void fibonacciDeUnoEsUno()
+    {
+        Fibonacci fibonacci = new Fibonacci();
+
+        int expectedValue = 1;
+        int obtainedValue = fibonacci.compute(1);
+
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
     public void fibonacciDeDosEsUno()
     {
         Fibonacci fibonacci = new Fibonacci();
 
         int expectedValue = 1;
         int obtainedValue = fibonacci.compute(2);
+
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void fibonacciDeDiezEsCincuentaYCinco()
+    {
+        Fibonacci fibonacci = new Fibonacci();
+
+        int expectedValue = 55;
+        int obtainedValue = fibonacci.compute(10);
 
         assertEquals(expectedValue, obtainedValue);
     }
